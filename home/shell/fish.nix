16d5eee0e5dc09e1df5d46cd794d9bf71fb03174
Zerodya <npagliuca2001@protonmail.com>
2024-05-27 19:36:23 +0200

    feat: add fish functions

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -6,11 +6,42 @@
     '';
 
     functions = {
-      nixos-rebuild = "sudo nixos-rebuild $argv &| nom && printf '\\n' && nvd diff (ls -d1v /nix/var/nix/profiles/system-*-link | tail -n 2);";
+      eva-rebuild = {
+        # Assumes flake is located in ~/nix-config/
+        body = ''
+          if test (count $argv) -ne 2
+              echo "Usage: eva-rebuild <nixos-rebuild-arg> <system-name>"
+              return 1
+          end
+
+          set rebuild_arg $argv[1]
+          set system_name $argv[2]
+
+          sudo nixos-rebuild $rebuild_arg --flake "nix-config/#$system_name" &| nom
+          and printf '\\n'
+          and nvd diff (ls -d1v /nix/var/nix/profiles/system-*-link | tail -n 2)
+        '';
+      };
+
+      eva-update = {
+        # Assumes flake is located in ~/nix-config/
+        body = ''
+          if test (count $argv) -ne 2
+              echo "Usage: eva-update <nixos-rebuild-arg> <system-name>"
+              return 1
+          end
+
+          set rebuild_arg $argv[1]
+          set system_name $argv[2]
+
+          sudo nix flake update 'nix-config/'
+          and eva-rebuild $rebuild_arg $system_name
+        '';
+      };
     };
   };
-
+  
   programs.starship.enable = true;
   programs.starship.enableFishIntegration = true;
-
+  
 }
