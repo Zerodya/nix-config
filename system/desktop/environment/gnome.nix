afea6179dd5d0a82be1d0083e72ed77d002a3f34
Zerodya <npagliuca2001@protonmail.com>
2024-07-30 21:38:17 +0200

    refactor: edit kde and gnome unnecessary packages

--- system/desktop/environment/gnome.nix
+++ system/desktop/environment/gnome.nix
@@ -10,30 +10,33 @@
     gnome-browser-connector
     gnome-online-accounts
     gnome-user-docs
+    gnome-user-share
+    gnome-font-viewer
+    gnome-system-monitor
+    gnome-terminal
+    epiphany # web browser
+    geary # email reader
+    evince # document viewer
+    totem # video player
   ]) ++ (with pkgs.gnome; [
     gnome-software
     gnome-music
-    gnome-user-share
     gnome-clocks
     gnome-maps
     gnome-weather
     gnome-online-miners
     gnome-contacts
-    gnome-font-viewer
-    gnome-system-monitor
     gnome-initial-setup
     gnome-music
-    gnome-terminal
-    epiphany # web browser
-    geary # email reader
-    evince # document viewer
     gnome-characters
-    totem # video player
     tali # poker game
     iagno # go game
     hitori # sudoku game
     atomix # puzzle game
   ]);
+
+  # Fix `programs.ssh.askPassword` conflict between Gnome's `seahorse` and Plasma's `ksshaskpass`
+  programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.seahorse.out}/libexec/seahorse/ssh-askpass";
 
   # Gnome theme for Qt apps
   qt = {
--- system/desktop/environment/plasma.nix
+++ system/desktop/environment/plasma.nix
@@ -3,11 +3,61 @@
 {
   services.desktopManager.plasma6.enable = true;
 
-  # Exclude some KDE packages
+  # Exclude unnecessary KDE packages
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    plasma-browser-integration
+    # Utilities
+    dolphin
     konsole
+    kate
+    kcalc
+    okular
+    gwenview
+    ark
+    kruler
+    kfind
+    kcharselect
+    spectacle
+
+    # Multimedia
+    dragon
+    elisa
+    kdenlive
+
+    # Graphics
+    kolourpaint
+
+    # Internet
+    falkon
+    kmail
+    konversation
+    ktorrent
+
+    # Office
+    korganizer
+
+    # Development
+    kdevelop
+
+    # Education
+    kalgebra
+    marble
+    kgeography
+    ktouch
+
+    # Other
+    plasma-browser-integration
     oxygen
+
+    # Additional packages
+    baloo           # File indexing and search
+    kmag            # Screen magnifier
+    kmouth          # Speech synthesizer front-end
+    kmousetool      # Automatic mouse click
+    knotes          # Notes application
+    krdc            # Remote desktop client
+    krfb            # Remote desktop server
+    kwalletmanager  # Wallet management tool
+    yakuake         # Drop-down terminal
   ];
 
   # Gnome theme for Qt apps
