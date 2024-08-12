2bca7ca88dcee747b24e305a4d0dc453234182fa
Zerodya <npagliuca2001@protonmail.com>
2024-08-12 17:50:11 +0200

    refactor: hardware-configuration

--- hosts/desktop/eva01/hardware-configuration.nix
+++ hosts/desktop/eva01/hardware-configuration.nix
@@ -10,50 +10,52 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/415ade6f-9049-4c24-8cf1-c15f2888f330";
-      fsType = "btrfs";
-      options = [ "subvol=root" "compress=zstd" ];
-    };
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/415ade6f-9049-4c24-8cf1-c15f2888f330";
-      fsType = "btrfs";
-      options = [ "subvol=home" "compress=zstd" ];
-    };
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/415ade6f-9049-4c24-8cf1-c15f2888f330";
-      fsType = "btrfs";
-      options = [ "subvol=nix" "compress=zstd" "noatime" ];
-    };
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/4676-F685";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
+  fileSystems = {
+    "/" = { 
+        device = "/dev/disk/by-uuid/415ade6f-9049-4c24-8cf1-c15f2888f330";
+        fsType = "btrfs";
+        options = [ "subvol=root" "compress=zstd" "noatime" ];
+      };
+    "/home" = { 
+        device = "/dev/disk/by-uuid/415ade6f-9049-4c24-8cf1-c15f2888f330";
+        fsType = "btrfs";
+        options = [ "subvol=home" "compress=zstd" "noatime"];
+      };
+    "/nix" = { 
+        device = "/dev/disk/by-uuid/415ade6f-9049-4c24-8cf1-c15f2888f330";
+        fsType = "btrfs";
+        options = [ "subvol=nix" "compress=zstd" "noatime" ];
+      };
+    "/boot" = { 
+        device = "/dev/disk/by-uuid/4676-F685";
+        fsType = "vfat";
+        options = [ "fmask=0022" "dmask=0022" ];
+      };
 
-  # Linux HDD Storage
-  fileSystems."/mnt/linuxdisk" = {
-    device = "UUID=fae6a6f8-30e1-4720-8f9e-03e1b10fff3c";
-    fsType = "ext4";
-    options = ["rw" "user" "exec"];
-  };
-  # Linux NVMe Storage
-  fileSystems."/mnt/linuxdisk2" = {
-    device = "UUID=0d15747d-6d76-4c5c-9639-50b680ce95fb";
-    fsType = "ext4";
-    options = ["rw" "user" "exec"];
-  };
-  # Windows NVMe Root
-  fileSystems."/mnt/winroot" = {
-    device = "UUID=20E6BB03E6BAD86C";
-    fsType = "ntfs-3g";
-    options = ["uid=1000" "gid=1000" "rw" "user" "exec" "umask=000"];
-  };
-  # Windows HDD Storage
-  fileSystems."/mnt/windisk" = {
-    device = "UUID=54604BAA604B91A2";
-    fsType = "ntfs-3g";
-    options = ["uid=1000" "gid=1000" "rw" "user" "exec" "umask=000"];
+    # Linux HDD Storage
+    "/mnt/linuxdisk" = {
+      device = "UUID=10f06b70-d877-492f-87c1-a4e7b547a970";
+      fsType = "btrfs";
+      options = [ "nofail" "rw" "user" "exec" "compress=zstd" "noatime" ];
+    };
+    # Linux NVMe Storage
+    "/mnt/linuxdisk2" = {
+      device = "UUID=df9f90e9-b2ed-4f7e-b353-f881fe97079f";
+      fsType = "btrfs";
+      options = [ "nofail" "rw" "user" "exec" "compress=zstd" "noatime" ];
+    };
+    # Windows NVMe Root
+    "/mnt/winroot" = {
+      device = "UUID=20E6BB03E6BAD86C";
+      fsType = "ntfs-3g";
+      options = [ "nofail" "uid=1000" "gid=1000" "rw" "user" "exec" "umask=000" ];
+    };
+    # Windows HDD Storage
+    "/mnt/windisk" = {
+      device = "UUID=54604BAA604B91A2";
+      fsType = "ntfs-3g";
+      options = [ "nofail" "uid=1000" "gid=1000" "rw" "user" "exec" "umask=000" ];
+    };
   };
 
   swapDevices =
