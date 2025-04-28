2ade514fb6368b1aa7f707de0325f15565a096fa
Zerodya <npagliuca2001@protonmail.com>
2025-04-28 22:29:13 +0200

    feat: initial Immich commit

--- hosts/server/photos/default.nix
+++ hosts/server/photos/default.nix
@@ -1,4 +1,4 @@
-{ photos-server, ... }:
+{ photos-server, username, ... }:
 
 {
   imports = [
@@ -9,5 +9,40 @@
   ];
 
   networking.hostName = photos-server;
+
+  # Immich photos
+  services.immich = {
+    enable = true;
+    host = "0.0.0.0";
+    port = 4470;
+    openFirewall = true;
+    secretsFile = "/home/${username}/nix-config/hosts/server/photos/immich.env";
+  };
+
+  # Backup
+  services.borgbackup = {
+    jobs = {
+      immich = {
+        # What to back up
+        paths = [ "/var/lib/immich" ];
+        # Where to store the Borg repository
+        repo = "/mnt/backups/105/immich";
+
+        encryption = {
+          mode = "repokey-blake2";
+          passCommand = "cat /home/${username}/nix-config/hosts/server/photos/borg.key";
+        };
+
+        compression = "auto,zstd,9";
+
+        removableDevice = true;
+
+        # Keep 1 backup every 7 days
+        startAt = "7d";
+        prune.keep = { daily = 1; };
+        persistentTimer = true;
+      };
+    };
+  };
 
 }
--- hosts/server/photos/home.nix
+++ hosts/server/photos/home.nix
@@ -3,11 +3,5 @@
   imports = [
     ../../../system/server/common/home.nix
   ];
-
-
-  home.file."docker-compose/immich-app" = {
-     source = ../../../docker-compose/immich-app;
-     executable = true;
-  };
   
 }
