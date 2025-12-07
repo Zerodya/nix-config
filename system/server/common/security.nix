583d545289db9f0211aea641340dd8d16faebc3e
Zerodya <npagliuca2001@protonmail.com>
2025-12-07 15:49:42 +0100

    refactor: server ssh security

--- system/server/common/security.nix
+++ system/server/common/security.nix
@@ -1,6 +1,7 @@
+{ username, ... }:
 {
-  # Disable sudo
-  # security.sudo.enable = false;
+  # Sudo hardening
+  security.sudo.execWheelOnly = true;
 
   # Only allow root to use the Nix package manager
   # nix.settings.allowed-users = [ "root" ];
@@ -13,13 +14,17 @@
   #  "/var/log".options = [ "noexec" ];
   #};
 
-  # Limit ssh
+  # SSH hardening
   services.openssh = {
+    enable = true;
+    ports = [ 5432 ];
     settings = {
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      AllowUsers = [ "${username}" ];
     };
-    allowSFTP = false;
+    allowSFTP = true;
     extraConfig = ''
       AllowTcpForwarding yes
       X11Forwarding no
@@ -28,9 +33,12 @@
       AuthenticationMethods publickey
     '';
   };
-
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBtTV2MdD+mP0Ukk98OKRyFWPL4kzkH4mUtOwqUUZ277 alpha@eva01"
+  users.users.${username}.openssh.authorizedKeys.keys = [
+    # Generate a public key in each host: `ssh-keygen -t ed25519 -f ~/.ssh/id_SERVERNAME`
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBtTV2MdD+mP0Ukk98OKRyFWPL4kzkH4mUtOwqUUZ277 alpha@eva01" # Desktop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKwgs9nKpLG60t+AMXQ0R57NAWGaYtKMua+/mO8KNAV9 u0_a235@localhost" # Phone
   ];
+
+  #services.fail2ban.enable = true;
 
 }
