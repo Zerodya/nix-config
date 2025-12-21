1cc4c83c49628017a7f5712c1d505762046824f5
Zerodya <npagliuca2001@protonmail.com>
2025-12-21 13:04:18 +0100

    fix: hyprland version mismatch

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,6 +1,10 @@
-{ pkgs, ...}: {
+{ pkgs, inputs, ...}: {
 
-  wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland = {
+    enable = true; 
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
   
   # Extra packages
   home.packages = with pkgs; [
@@ -33,7 +37,7 @@
 
   # Plugins
   wayland.windowManager.hyprland.plugins = [
-    pkgs.hyprlandPlugins.hyprscrolling
+    inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprscrolling
   ];
 
   # Main configuration
