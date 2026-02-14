69d9c7d7f956edcca77c9dabc12f7600dbed7903
Zerodya <npagliuca2001@protonmail.com>
2026-02-14 18:59:18 +0100

    feat: add support for GLaDOS local llm

--- system/modules/ollama-rocm.nix
+++ system/modules/ollama-rocm.nix
@@ -5,14 +5,35 @@
   services.ollama = {
     enable = true;
     package = pkgs.ollama-rocm;
+    loadModels = [ "llama3.2" ];
   };
 
-  # ROCm
   hardware.amdgpu.opencl.enable = true;
+
   environment.systemPackages = with pkgs; [
+    # ROCm
     rocmPackages.rocm-core
     rocmPackages.rocm-runtime
     rocmPackages.rocm-smi
     rocmPackages.rocminfo
+
+    # TTS
+    nur.repos.xddxdd.openedai-speech
+
+    # Other tools
+    uv
   ];
+
+  # GLaDOS at https://github.com/dnhkng/GLaDOS
+  programs.nix-ld = {
+    enable = true;
+    # Inside the cloned repo:
+    # Install with: `LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH steam-run python scripts/install.py`
+    # Run with: `LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH steam-run uv run glados tui`
+    libraries = with pkgs; [
+      portaudio
+      onnx
+      onnxruntime
+    ];
+  };
 }
