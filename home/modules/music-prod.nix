6257987e755970f8a52a9ec0c9f7e7aed6213e00
Zerodya <npagliuca2001@protonmail.com>
2026-04-01 15:28:01 +0200

    fix: reaper extensions

--- home/modules/music-prod.nix
+++ home/modules/music-prod.nix
@@ -1,8 +1,25 @@
 { pkgs, ... }:
 
+let
+  # Some Reapack extensions need this to work
+  reaper-wrapped = pkgs.symlinkJoin {
+    name = "reaper";
+    paths = [ pkgs.reaper ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/reaper \
+        --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [
+          pkgs.gtk3 pkgs.gdk-pixbuf pkgs.libepoxy pkgs.fontconfig
+          pkgs.freetype pkgs.libpng pkgs.zlib pkgs.glib pkgs.cairo
+          pkgs.pango pkgs.harfbuzz pkgs.libjpeg pkgs.stdenv.cc.cc
+        ]}"
+    '';
+  };
+in
+
 {
   home.packages = with pkgs; [
-    reaper
+    reaper-wrapped
     
     tonelib-gfx-478
 
