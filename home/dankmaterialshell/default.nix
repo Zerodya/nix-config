952c1a7a9180ec63489452516dfffc942b4b74b3
Zerodya <npagliuca2001@protonmail.com>
2026-09-07 21:49:40 +0200

    feat: add dms search

--- home/dankmaterialshell/default.nix
+++ home/dankmaterialshell/default.nix
@@ -1,7 +1,11 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 let
   flakePath = "${config.home.homeDirectory}/nix-config"; 
 in
 {
   home.file.".config/DankMaterialShell/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${flakePath}/home/dankmaterialshell/settings.json";
+
+  home.packages = with pkgs; [
+    dsearch
+  ];
 }
